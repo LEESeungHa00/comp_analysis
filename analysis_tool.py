c56b1f24cebad4bdd57d166d530d905e2842f4ae
SeungHa <devseungha000@gmail.com>
2025-09-11 09:42:41 +0900

    Update analysis_tool.py

--- analysis_tool.py
+++ analysis_tool.py
@@ -420,11 +420,11 @@ if selected == "고객사 효율 분석":
                         related_clusters = after_df[after_df['origin_country'] == origin]['product_name'].unique()
                         new_origin_details.append({
                             '신규 원산지': origin,
-                            '관련 품목군': '\n '.join(related_clusters)
+                            '관련 품목명': '\n'.join(related_clusters)
                         })
                     st.dataframe(pd.DataFrame(new_origin_details))
-                else:
-                    st.info("계약 이후 새로 추가된 원산지는 없습니다.")
+            else:
+                st.info("계약 이후 새로 추가된 원산지는 없습니다.")
             
             st.markdown("---")
 
@@ -441,7 +441,7 @@ if selected == "고객사 효율 분석":
                         related_clusters = after_df[after_df['Exporter'] == exporter]['product_name'].unique()
                         new_exporter_details.append({
                             '신규 공급사': exporter,
-                            '관련 품목군': '\n '.join(related_clusters)
+                            '관련 품목명': '\n'.join(related_clusters)
                         })
                     st.dataframe(pd.DataFrame(new_exporter_details))
                 else:
@@ -793,4 +793,5 @@ if selected == "시장 경쟁력 분석":
                                     st.write("- 더 저렴한 원산지 없음")
         else:
             st.warning("'Exporter' 또는 'Origin Country' 컬럼이 없어 공급망 분석을 수행할 수 없습니다.")
+
 
